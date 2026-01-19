4f42b49952112487c8cd8e9ce1d81025bc3a9c75
Craigy131 <65000316+Trey16Miller@users.noreply.github.com>
2026-01-19 03:53:25 -0500

    Create sh_config.lua

---
+++ sh_config.lua
@@ -0,0 +1,13 @@
+SCP1234 = SCP1234 or {}
+
+SCP1234.BlinkInterval = 3
+SCP1234.BlinkDuration = 0.45
+
+SCP1234.ObserveFOVDeg = 70
+SCP1234.ObserveMaxDist = 6000
+
+SCP1234.PetrifyRange = 70
+SCP1234.PetrifyKillDelay = 10
+
+SCP1234.StoneMat = "models/props_wasteland/rockcliff02a"
+SCP1234.StoneCol = Color(150, 150, 150)
